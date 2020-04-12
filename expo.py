d6466677bd5b464f6e81b97564c7360a579f02c9
Ali Jaber (alijabe) <alijabe@cisco.com>
2020-04-13 00:26:51 +0400

    correct one

---
+++ expo.py
@@ -0,0 +1,26 @@
+import getpass
+import sys
+import telnetlib
+from netmiko import ConnectHandler
+
+with open('commands_file') as f:
+    commands_list = f.read().splitlines()
+
+with open('devices_file') as f:
+    devices_list = f.read().splitlines()
+
+for devices in devices_list:
+    print ('Connecting to device" ' + devices)
+    ip_address_of_device = devices
+    iosxe_device = {
+        'device_type': 'cisco_iosxe',
+        'ip': ip_address_of_device,
+        'username': 'cisco',
+        'password': 'cisco'
+    }
+
+    net_connect = ConnectHandler(**iosxe_device)
+    output = net_connect.send_config_set(commands_list)
+    tn.read_until(b"This operation may require a reload of the system. Do you want to proceed? [y/n] ")
+    tn.write(b"y\n")
+    print (output)
